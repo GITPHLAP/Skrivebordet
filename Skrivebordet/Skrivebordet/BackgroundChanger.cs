356bf8b049e06988082b08c096019a9b82257c6c
Philip K. Larsen <kjedtoft@gmail.com>
2022-04-20 10:00:31 +0200

    First commit

---
+++ Skrivebordet/Skrivebordet/BackgroundChanger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skrivebordet
+{
+    public class BackgroundChanger
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, String pvParam, uint fWinIni);
+
+        private const uint SPI_SETDESKWALLPAPER = 0x14;
+
+        public static void ChangeWallpaper(string path)
+        {
+            uint flags = 0;
+            if (!SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                    0, path, flags))
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
---
+++ Skrivebordet/Skrivebordet/ImageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skrivebordet
+{
+    internal class ImageHandler
+    {
+        public void Load(string filePath)
+        {
+
+        }
+
+
+    }
+}
